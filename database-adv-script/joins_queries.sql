b9d72fbce4fb03e6bb024d77212fd1763588ead8
NobelAssefa <87548061+NobelAssefa@users.noreply.github.com>
2025-05-13 10:04:41 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,20 @@
+//INNER_JOIN
+SELECT 
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    b.created_at AS booking_created_at,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    u.phone_number,
+    u.role
+FROM 
+    Booking b
+INNER JOIN 
+    User u ON b.user_id = u.user_id;
 
