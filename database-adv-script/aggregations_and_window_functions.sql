fb23ff8649c921c95a441732f7f9d84f446458b6
NobelAssefa <87548061+NobelAssefa@users.noreply.github.com>
2025-05-13 11:42:47 +0300

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -13,7 +13,7 @@ LEFT JOIN
 GROUP BY 
     u.user_id, u.first_name, u.last_name, u.email;
 
-//Windows_function//
+//Windows_function RANK()//
 
 SELECT 
     p.property_id,
@@ -26,3 +26,17 @@ LEFT JOIN
     Booking b ON p.property_id = b.property_id
 GROUP BY 
     p.property_id, p.name;
+//Windows_function ROW_NUMBER()//
+
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    COUNT(b.booking_id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_row_number
+FROM 
+    Property p
+LEFT JOIN 
+    Booking b ON p.property_id = b.property_id
+GROUP BY 
+    p.property_id, p.name;
+
