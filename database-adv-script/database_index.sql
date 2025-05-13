e9d5e77bd8efc9bbd24daea22d9bc75c0d7be93a
NobelAssefa <87548061+NobelAssefa@users.noreply.github.com>
2025-05-13 11:52:19 +0300

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,1 +1,19 @@
 
+
+CREATE INDEX idx_user_email ON User(email);
+CREATE INDEX idx_user_id ON User(user_id);
+CREATE INDEX idx_user_role ON User(role);
+CREATE INDEX idx_user_created_at ON User(created_at);
+
+CREATE INDEX idx_booking_user_id ON Booking(user_id);
+CREATE INDEX idx_booking_property_id ON Booking(property_id);
+CREATE INDEX idx_booking_status ON Booking(status);
+CREATE INDEX idx_booking_start_date ON Booking(start_date);
+CREATE INDEX idx_booking_end_date ON Booking(end_date);
+CREATE INDEX idx_booking_created_at ON Booking(created_at);
+
+
+CREATE INDEX idx_property_host_id ON Property(host_id);
+CREATE INDEX idx_property_location ON Property(location);
+CREATE INDEX idx_property_pricepernight ON Property(pricepernight);
+CREATE INDEX idx_property_created_at ON Property(created_at);
