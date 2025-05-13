74de30a1fe5e095f2bd81312b43a7a0be884b4b5
NobelAssefa <87548061+NobelAssefa@users.noreply.github.com>
2025-05-13 10:15:46 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -18,3 +18,41 @@ FROM
 INNER JOIN 
     User u ON b.user_id = u.user_id;
 
+//LEFT_JOIN//
+
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    p.description,
+    p.location,
+    p.pricepernight,
+    r.review_id,
+    r.user_id AS reviewer_id,
+    r.rating,
+    r.comment,
+    r.created_at AS review_created_at
+FROM 
+    Property p
+LEFT JOIN 
+    Review r ON p.property_id = r.property_id;
+
+
+//OUTER_JOIN//
+
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    b.created_at AS booking_created_at
+FROM 
+    User u
+FULL OUTER JOIN 
+    Booking b ON u.user_id = b.user_id;
+
