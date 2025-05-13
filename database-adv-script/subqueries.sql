882764a5c34a2911dcd114554d555111a20f709a
NobelAssefa <87548061+NobelAssefa@users.noreply.github.com>
2025-05-13 11:06:47 +0300

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,36 @@
+//Sub_query 
 
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    p.description,
+    p.location,
+    p.pricepernight
+FROM 
+    Property p
+WHERE 
+    p.property_id IN (
+        SELECT 
+            r.property_id
+        FROM 
+            Review r
+        GROUP BY 
+            r.property_id
+        HAVING 
+            AVG(r.rating) > 4.0
+    );
+
+//Coorelated_subquery 
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email
+FROM 
+    User u
+WHERE 
+    (
+        SELECT COUNT(*) 
+        FROM Booking b 
+        WHERE b.user_id = u.user_id
+    ) > 3;
